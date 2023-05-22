be771af13e52def3c142a9a2d4472c0da48a6310
BELEN MARZO GASCA <a178979@alliance-healthcare.com>
2023-05-22 17:49:20 +0200

    RootReducer

--- src/index.js
+++ src/index.js
@@ -5,11 +5,9 @@ import App from './App'
 import reportWebVitals from './reportWebVitals'
 import { BrowserRouter } from "react-router-dom"
 
-import {createStore} from 'redux'
 import {Provider} from 'react-redux'
-import reducer from './reducers/contactsReducer'
+import store from './store'
 
-const store = createStore(reducer)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
